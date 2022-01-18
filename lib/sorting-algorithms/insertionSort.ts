c68e19db1556847cd95df3de4d5778cfb178e32d
Mehar Gaur <mehar.sg.m@gmail.com>
2022-01-18 17:41:39 -0500

    🚀 Full implementation of insertion sort along with animation logic

--- lib/sorting-algorithms/insertionSort.ts
+++ lib/sorting-algorithms/insertionSort.ts
@@ -9,10 +9,12 @@ async function insertionSort (boxes: Box[ ]) {
     // Loop through unsorted elements, assume first is sorted
     for (let i = 1; i < boxes.length; i++) {
         const currentBoxHeight = boxes[i].geometry.parameters.height
+        
+        await delay(100)
 
-        await gsap.to(boxes[i].position, {
-            y: `-=${ currentBoxHeight }`
-        })
+        await moveBox(boxes[i], '-', currentBoxHeight)
+
+        await delay(100)
 
         // Insert currentBox at the right position in the sorted section
         for (let j = i - 1; j >= 0; j--) {
@@ -22,22 +24,14 @@ async function insertionSort (boxes: Box[ ]) {
                 // Reset boxes back to resting color
                 await resetBoxes(boxes[j], boxes[j + 1])
 
-                // If j == 0, insert it here as loop exits right after this
+                // If j == 0, raise it up now as loop exits right after this
                 if (j == 0) {
-                    await gsap.to(boxes[0].position, {
-                        y: `+=${ currentBoxHeight }`
-                    })
-                    // boxes[0].position.y += currentBoxHeight
-                    // await delay(100)
+                    await moveBox(boxes[0], '+', currentBoxHeight)
                 }
             }
             else {
                 // Insert the current box here and break inner loop
-                await gsap.to(boxes[j + 1].position, {
-                    y: `+=${ currentBoxHeight }`
-                })
-                // boxes[j + 1].position.y += currentBoxHeight
-                // await delay(100)
+                await moveBox(boxes[j + 1], '+', currentBoxHeight)
                 break
             }
         }
@@ -47,6 +41,15 @@ async function insertionSort (boxes: Box[ ]) {
 
     // Make the boxes "dance" on complete
     await staggerBoxes(boxes)
+}
+
+
+function moveBox(box: Box, direction: '+' | '-', amount: number) {
+    return gsap.to(box.position, {
+        ease: 'power2.out',
+        y: `${ direction }=${ amount }`,
+        duration: STEP_DELAY / 1000
+    })
 }
 
 
