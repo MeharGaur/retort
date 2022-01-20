e4ae0c7b6ba75ca3553b8557f456e6269ba3913e
Mehar Gaur <mehar.sg.m@gmail.com>
2022-01-20 15:48:31 -0500

    ⛓ Basic quick sort implementation that can sort Box arrays

--- lib/sorting-algorithms/quickSort.ts
+++ lib/sorting-algorithms/quickSort.ts
@@ -1,6 +1,46 @@
+import { STEP_DELAY } from "../config"
+import type { Box } from "../types"
+import { delay, getHeight, staggerBoxes, syncBoxPositions } from "../utils"
 
-function quickSort () {
-    console.log('xddd')
+
+async function quickSort(boxes: Box[ ]) {
+    const sortedBoxes = _quickSort(boxes)
+
+    boxes.length = 0
+    boxes.push(...sortedBoxes)
+
+    
 }
+
+
+/** Internal quick sort function */
+function _quickSort (boxes: Box[ ]) {
+    // Base case
+    if (boxes.length <= 1) {
+        return boxes
+    }
+
+    // Recursive case- sort using pivot
+    const pivotIndex = boxes.length - 1
+    
+    const smaller: Box[ ] = [ ] 
+    const larger: Box[ ] = [ ] 
+
+    for (const box of boxes) {
+        if (getHeight(box) < getHeight(boxes[pivotIndex])) {
+            smaller.push(box)
+        }
+        else {
+            larger.push(box)
+        }
+    }
+
+    return [
+        ..._quickSort(smaller), 
+        boxes[pivotIndex],
+        ..._quickSort(larger)
+    ]
+}
+
 
 export default quickSort
